03100932099d916ebdfdc1c840e7ba5fa6a694be
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-05-10 18:52:25 +0300

    Update staking.js

--- assets/js/staking.js
+++ assets/js/staking.js
@@ -12,7 +12,7 @@ if (typeof window.ethereum !== 'undefined') {
     web3 = new Web3(window.web3.currentProvider);
     //enable();
 	web3.eth.net.getId().then(console.log);
-  //getAccounts().then(variable => accounts = variable);
+  getAccounts().then(variable => accounts = variable);
 }
 
 
