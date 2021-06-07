0dad91974b79f14df1dcaa4925a91623890adf36
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-06-07 20:40:48 +0300

    Update staking.js

--- assets/js/staking.js
+++ assets/js/staking.js
@@ -1082,6 +1082,24 @@ else{
 		else{document.getElementById("status").innerHTML = "No FT to re-stake";}
 	}
 
+	function toFixed(x) {
+		if (Math.abs(x) < 1.0) {
+		  var e = parseInt(x.toString().split('e-')[1]);
+		  if (e) {
+			  x *= Math.pow(10,e-1);
+			  x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+		  }
+		} else {
+		  var e = parseInt(x.toString().split('+')[1]);
+		  if (e > 20) {
+			  e -= 20;
+			  x /= Math.pow(10,e);
+			  x += (new Array(e+1)).join('0');
+		  }
+		}
+		return x;
+	  }
+
 	var myVar = setInterval(displayID, 10000);
 	var myVar0 = setInterval(showAddress, 3000);
 	var myVar1 = setInterval(loadStats, 3000);
@@ -1091,8 +1109,8 @@ else{
 	}
 	var statictics;
 	function displayStats(){
-		document.getElementById("staked").innerHTML = "Staked: " + statictics[0]/10000000000;
-		document.getElementById("rewards").innerHTML = "Rewards: " + statictics["rewards"]/10000000000;
+		document.getElementById("staked").innerHTML = "Staked: " + toFixed(statictics[0]/10000000000);
+		document.getElementById("rewards").innerHTML = "Rewards: " + toFixed(statictics["rewards"]/10000000000);
 	}
 	function loadStats(){
 		getStats(accounts[0]).then(stats => statictics = stats);
