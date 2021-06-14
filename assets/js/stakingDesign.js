924074dec8484add43c2f360582d2e3e6403644b
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-06-14 10:50:01 +0300

    Add files via upload

---
+++ assets/js/stakingDesign.js
@@ -0,0 +1,8 @@
+
+function displayUnstake(){
+    document.getElementById("stake").innerHTML = document.getElementById("stake2").innerHTML;
+}
+
+function displayStake(){
+    document.getElementById("stake").innerHTML = document.getElementById("stake1").innerHTML;
+}
