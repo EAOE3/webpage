1769e612fb530092d87f80fd0d8db0aabc817dbc
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-06-18 21:16:00 +0300

    Update bridge.js

--- assets/js/bridge.js
+++ assets/js/bridge.js
@@ -39,7 +39,7 @@ ethereum.on('accountsChanged', function getAccounts() {
   }
   
   async function getAccounts(){
-		getMetMaskAccounts().then(variable => accounts = variable).then(console.log(accounts[0]));
+		getMetMaskAccounts().then(variable => accounts = variable).then(console.log(accounts));
       return accounts;
     }
 
