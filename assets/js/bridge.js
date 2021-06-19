54fb82603f281e814f1db7f7feb66c71c14e6d80
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-06-19 19:45:17 +0300

    Update bridge.js

--- assets/js/bridge.js
+++ assets/js/bridge.js
@@ -69,6 +69,6 @@ ethereum.on('accountsChanged', function getAccounts() {
   }
 
   async function getAllowance(contract, user) {
-    const tokenContract = new web3.eth.Contract(ERC20abi,contract);
-    return tokenContract.methods.balanceOf(user).call();
+    //const tokenContract = new web3.eth.Contract(ERC20abi,contract);
+    return thisContract.methods.balanceOf(user).call();
   }
