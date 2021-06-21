74048e61c8c7839c9b7375a99323ff736dc8949d
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-06-21 12:08:45 +0300

    Update bridge.js

--- assets/js/bridge.js
+++ assets/js/bridge.js
@@ -35,6 +35,7 @@ ethereum.on('accountsChanged', function getAccounts() {
   const thisContract = new web3.eth.Contract(ERC20abi,'0xc8aa1adc636d2369f3c9e94fef0705e2b2ba235c');
 
   getFTbalance().then(bal => console.log(bal));
+  getMetMaskAccounts().then(val => console.log(val[0]));
   getAllowance("0xc8aa1adc636d2369f3c9e94fef0705e2b2ba235c", "0x2DBE938e546357D9738087C39C54C96FEdb23e11").then(bal => console.log(bal));
   //console.log(getAllownce());
   //getAllownce().then(console.log);
