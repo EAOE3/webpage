ff74fa9a6926a58339f49a15d521ab660450addd
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-06-22 19:37:29 +0300

    Update bridge.js

--- assets/js/bridge.js
+++ assets/js/bridge.js
@@ -12,7 +12,7 @@ async function enable(){
       }
 
 if (typeof window.ethereum !== 'undefined') {
-  
+  console.log(window.ethereum);
   console.log('MetaMask is installed!');
     web3 = new Web3(window.web3.currentProvider);
     enable();
