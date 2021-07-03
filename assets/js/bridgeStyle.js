7e70e0153e300f8d1d3e1a92713da7726349c75d
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-07-03 17:56:11 +0300

    Update bridgeStyle.js

--- assets/js/bridgeStyle.js
+++ assets/js/bridgeStyle.js
@@ -1,89 +1,101 @@
-var selectedTokenSymbol;
-var tokensSavedData = "";
+<!DOCTYPE html>
+<html lang="en">
 
-startTokensLoading();
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-function showTokens() {
-    document.getElementById("token-choose").style.visibility = "visible";
-  }
-  function hideTokens() {
-    document.getElementById("token-choose").style.visibility = "hidden";
-  }
+  <title>Family Token Staking</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-  function addToken(tokenAddress, symbol, name) {
+  <!-- Favicons -->
+  <link href="assets/img/FC.png" rel="icon" type="image/png">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-    console.log("huh");
-    
-    let div = document.createElement('div');
-    let hr = document.createElement('hr');
-    let img = document.createElement('img');
-    let div1 = document.createElement('div');
-    let div2 = document.createElement('div');
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    div.id = 'token-token';
-    hr.id = 'token-token-empty';
-    img.id = 'token-token-icon';
-    div1.id = 'token-token-symbol';
-    div2.id = 'token-token-name';
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
 
-    img.src = "assets\\img\\" + symbol + ".png"
-    div1.innerHTML = symbol;
-    div2.innerHTML = name;
+  <!-- Template Main CSS File -->
+  <link href="assets/css/bridgeCSS.css" rel="stylesheet">
 
-    div.onclick = function(event) {setToken(tokenAddress, symbol);}
-
-    //div.onclick = setToken(tokenAddress, symbol);
-
-    div.appendChild(hr);
-    div.appendChild(img);
-    div.appendChild(div1);
-    div.innerHTML += "<br/>";
-    div.appendChild(div2);
-
-    console.log("wtf");
-    document.getElementById('token-select').appendChild(div);
-    console.log("wtf1");
-
-  }
-
-  function setToken(tokenAddress, symbol) {
-    selectedToken = tokenAddress;
-    selectedTokenSymbol = symbol;
-    document.getElementById('token-icon').src = "assets/img/" + symbol + ".png";
-    document.getElementById('token-name').innerHTML = symbol;
-    document.getElementById('token-choose').style.visibility = "hidden";
-  }
-
-function startTokensLoading() {
-    readTextFile("assets/Tokens.txt");
-  }
   
-function readTextFile(file) {
+</head>
 
-    fetch('assets/Tokens.txt')
-  .then(response => response.text())
-  .then(text => loadTokens(text));
-}
+<body>
 
-function loadTokens(data) {
-  var newData = data + "";
-  tokensSavedData = newData;
-  var tokensData = newData.split(':');
+  <div id="token-choose">
+    <div id="token-choose-body">
+      <h3 id="select-a-token">Select a token</h3>
+      <img id="close-x" src="assets\img\close-x.png" onclick="hideTokens()">
+      <br/>
+      <hr style="height: 1px; color: aliceblue; margin-top: 35px;"/>
+      <input id="token-input" placeholder="Search by name or paste address" onkeypress="search()" onkeydown="search()">
 
-  for(t = 0; t < tokensData.length; t += 3){
-    addToken(tokensData[t], tokensData[t+1], tokensData[t+2]);
-  }
-}
+      <div id="token-select">
+      </div>
+    </div>
+  </div>
+  <div id="transfer">
 
-function search(){
-  document.getElementById("token-select").innerHTML = "";
-  console.log(tokensSavedData);
-  var tokensData = tokensSavedData.split(':');
-  var input = document.getElementById("token-input").value + "";
-  console.log(input);
-  for(t = 0; t < tokensData.length; t += 3){
-    if(tokensData[t+1].includes(input.toUpperCase())){
-      addToken(tokensData[t], tokensData[t+1], tokensData[t+2]);
-    }
-  }
-}
+    <select name="pets" id="bridge-select">
+      <option value="BSCETH">BSC->ETH</option>
+      <option value="ETHBSC">ETH->BSC</option>
+  </select>
+  <br/>
+  <div id="token" onclick="showTokens()">
+    <img  id="token-icon" src="assets/img/FT.png"><h6 id="token-name">FT</h6>
+    <img id="downArrow" src="assets/img/white-transparent-arrow.png">
+  </div>
+
+    <input id="to" class="buttom" autocomplete="off">
+    <input id="amount" class="buttom" autocomplete="off">
+    <input id="fee" class="buttom" autocomplete="off">
+    <input id="claimingFee" class="buttom" autocomplete="off">
+    <br/><br/>
+    <center><input id="submit" type="submit" value="Transfer" style="width: 150px;" onclick="transfer()"></center>
+    <br/>
+    <center><h6 id="status"></h6></center>
+	<p style="text-align: center; margin-left: auto; margin-right: auto;"><h8 style="text-align: center;" id="address"></h8></p>
+  </div>
+  <br/>
+  <br/><br/>
+
+
+
+
+
+
+  <main id="main">
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+  <script type="text/javascript" src="https://unpkg.com/web3@latest/dist/web3.min.js"></script>
+  <script type="text/javascript" src=".\assets\js\bridge.js"></script>
+  <script type="text/javascript" src=".\assets\js\bridgeJSstyle.js"></script>
+
+</body>
+
+</html>
