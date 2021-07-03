cc10917d74b8f11123db47e1c51f08a14a75cd28
Crypto Family <81621551+CryptoFamily3@users.noreply.github.com>
2021-07-03 17:57:28 +0300

    Update bridge.js

--- assets/js/bridge.js
+++ assets/js/bridge.js
@@ -4,7 +4,7 @@ var accounts;
 var FTcontract = "0x1652557183585f1581A828FE59c9CbC551f774b8";
 var bridge = "0x025Ca32DA4430D22F9F6598e4e1b980B63171433";
 var selectedBridge = "BSCETH";
-var selectedToken;
+var selectedToken = "FTaddress";
 
 
 var allowance;
